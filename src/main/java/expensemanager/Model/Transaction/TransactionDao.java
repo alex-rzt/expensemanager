d76427972e5ddb66c3b4e713c555c6c02bdd28ef
alexjohnjacob <alex.john@razorthink.net>
2017-05-03 19:08:37 +0530

    Details commit

--- src/main/java/expensemanager/Model/Transaction/TransactionDao.java
+++ src/main/java/expensemanager/Model/Transaction/TransactionDao.java
@@ -1,12 +1,17 @@
 package expensemanager.Model.Transaction;
 
+import expensemanager.Model.Account.Account;
 import expensemanager.Model.Category.Category;
 import expensemanager.Model.User.User;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by alex on 24/4/17.
@@ -35,6 +40,23 @@ import org.springframework.transaction.annotation.Transactional;
         {
             getSession().update(transaction);
         }
+        public List<Transaction> getByUserId(int id)
+        {
+            Criteria criteria=getSession().createCriteria(Transaction.class);
+            criteria.add(Restrictions.eq("user_id",id));
+            List <Transaction> transactions=(List<Transaction>)criteria.list();
+            return  transactions;
+        }
+        public List<Transaction> getByAccountId(int id)
+        {
+            Account account=new Account();
+            account.setAccount_id(id);
+            Criteria criteria=getSession().createCriteria(Transaction.class);
+            criteria.add(Restrictions.eq("account",account));
+            List <Transaction> transactions=(List<Transaction>)criteria.list();
+            return  transactions;
+        }
+
 
     }
 
